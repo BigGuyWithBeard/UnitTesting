f44b8d2d2f664402d6475e81077574a133a64eed
John-Paul Smith <jp@johnpaulsmith.co.uk>
2024-08-15 16:16:38 +0100

    Added CustomerAccount and demo unit tests

---
+++ BigGuyWithBeard.UnitTesting/Part01/CustomerAccount.cs
@@ -0,0 +1,55 @@
+﻿namespace Part01
+{
+    /// <summary>
+    /// Customer Account demo class
+    /// </summary>
+    public class CustomerAccount
+    {
+        private readonly string m_customerName;
+        private double m_balance;
+
+
+        public CustomerAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+
+        public void Debit(double amount)
+        {
+            if (amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance += amount; // intentionally incorrect code
+        }
+
+        public void Credit(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance += amount;
+        }
+
+    }
+}
